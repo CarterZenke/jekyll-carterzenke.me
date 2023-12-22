78b6c5906908eb6d7f8614e8be8ca70ad11cb2a7
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2023-12-22 13:30:59 -0500

    handle missing keys in config

--- yaml_to_s3_json_config_converter.rb
+++ yaml_to_s3_json_config_converter.rb
@@ -5,25 +5,29 @@ require 'uri'
 def convert_yaml_to_s3_json_config(yaml_file, json_file)
   yaml_data = YAML.load_file(yaml_file)
 
-  redirect_rules = yaml_data['s3']['redirects'].map do |rule|
-    {
-      'Condition' => {
-        'KeyPrefixEquals' => rule['path'].delete_prefix('/')
-      },
-      'Redirect' => {
-        'HostName' => URI.parse(rule['destination']).host,
-        'HttpRedirectCode' => '301',
-        'Protocol' => URI.parse(rule['destination']).scheme,
-        'ReplaceKeyPrefixWith' => URI.parse(rule['destination']).path.delete_prefix('/')
-      }
-    }
-  end
+  website_configuration = {}
 
-  website_configuration = {
-    'ErrorDocument' => { 'Key' => yaml_data['s3']['error'] },
-    'IndexDocument' => { 'Suffix' => yaml_data['s3']['index'] },
-    'RoutingRules' => redirect_rules
-  }
+  if yaml_data['s3']
+    website_configuration['IndexDocument'] = { 'Suffix' => yaml_data['s3']['index'] } if yaml_data['s3']['index']
+    website_configuration['ErrorDocument'] = { 'Key' => yaml_data['s3']['error'] } if yaml_data['s3']['error']
+
+    if yaml_data['s3']['redirects']
+      redirect_rules = yaml_data['s3']['redirects'].map do |rule|
+        {
+          'Condition' => {
+            'KeyPrefixEquals' => rule['path'].delete_prefix('/')
+          },
+          'Redirect' => {
+            'HostName' => URI.parse(rule['destination']).host,
+            'HttpRedirectCode' => '301',
+            'Protocol' => URI.parse(rule['destination']).scheme,
+            'ReplaceKeyPrefixWith' => URI.parse(rule['destination']).path.delete_prefix('/')
+          }
+        }
+      end
+      website_configuration['RoutingRules'] = redirect_rules
+    end
+  end
 
   File.write(json_file, JSON.pretty_generate(website_configuration))
 end
